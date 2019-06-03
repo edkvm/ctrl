9934ea2f1374b4c10271183ed1f2f3c5606b4368
edkvm <edkvmn@gmail.com>
2019-06-04 01:17:58 +0300

    WIP

---
+++ examples/helloworld/handler.js
@@ -0,0 +1,9 @@
+
+	exports.myhandle = (event, callback) => {
+		console.log(process.version)
+        console.log("with socket")
+		console.log(event)
+		console.error("ryo")
+		console.log("helloworld")
+        callback(null, "objeYoct")
+	}
