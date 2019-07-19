c2b6fcd68468eea2d93b4607fbc1dca96d079030
edkvm <edkvmn@gmail.com>
2019-07-19 22:25:33 +0300

    fixed stall on error

--- examples/weather/__func__.js
+++ examples/weather/__func__.js
@@ -7,39 +7,63 @@ const http = require('http');
  */
 module.exports.main = (event, callback) => {
 
-	// Parse Event
-	let city = event.params.city;
-	let apiKey = event.$.config.apiKey;
-	let unitSys = event.$.config.unitSys;
+    // Parse Event
+    let city = event.params.city;
+    let apiKey = event.$.config.apiKey;
+    let unitSys = event.$.config.unitSys;
 
-	let url = buildUrl(city, unitSys, apiKey);
-	getWeather(url, callback);
+    let url = buildUrl(city, unitSys, apiKey);
+    getWeather(url)
+        .then(function(data) {
+            callback(`It's ${data.main.temp} degrees in ${data.name}!`, null);
+        })
+        .catch(function(err) {
+            callback(null, err);
+        });
 };
 
 function buildUrl(city, unitSys, apiKey) {
-	return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitSys}&appid=${apiKey}`;
+    return `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unitSys}&appid=${apiKey}`;
 }
 
-function getWeather(url, callback) {
-	http.get(url, (res) => {
-		let body = '';
+function getWeather(url) {
+    return new Promise(function(resolve, reject) {
+        http.get(url, (res) => {
+            const { statusCode } = res;
+            const contentType = res.headers['content-type'];
 
-		res.on('data', (chunk) => {
-			body += chunk;
-		})
+            let error;
+            if (statusCode !== 200) {
+                error = new Error('request failed.\n' +
+                    `status code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('invalid content-type.\n' +
+                    `expected application/json but received ${contentType}`);
+            }
 
-		res.on('end', () => {
-			if(res.statusCode === 200){
-				let weather = JSON.parse(body);
-				let answer = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-				callback(null, answer);
-			} else {
-				console.error(err);
-				callback(err, '');
-			}
+            if (error) {
+                reject(error);
+            }
 
-		})
+            res.setEncoding('utf8');
 
-	});
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+
+            res.on('end', () => {
+                try {
+                    let parsedData = JSON.parse(rawData);
+
+                    resolve(parsedData);
+                } catch (err) {
+                    reject(err);
+                }
+
+            })
+
+        }).on('error', (err) => {
+            reject(err);
+        });
+    });
 }
 
