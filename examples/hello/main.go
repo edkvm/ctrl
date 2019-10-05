bb2046d3764bd5be28be7aaadad6abefcd91d7f8
edkvm <edkvmn@gmail.com>
2019-10-05 15:35:15 +0300

    add rpc integration to go

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -1,58 +1,21 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/rpc"
+	"github.com/edkvm/ctrl/gowrapper"
 )
 
-type Handler struct {}
 
-func (handler *Handler) Action(data []byte, result *[]byte) error {
-
-	var params map[string]interface{}
-	err := json.Unmarshal(data, &params)
-	if err != nil {
-		return err
-	}
-
-	resp := []byte(fmt.Sprintf("Hello,%v", params["name"]))
-
-	result = &resp
-
-	return nil
+type Params struct{
+	Name string `json:"name"`
 }
 
-func Invoke(port int) {
-	h := &Handler{}
-
-	err := rpc.Register(h)
-	if err != nil {
-		log.Fatal()
-	}
-
-	rpc.HandleHTTP()
-
-	l, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
-
-	log.Println("listening on", port)
-	err = http.Serve(l, nil)
-	if err != nil {
-
-	}
+func Hello(params Params) (string, error) {
+	return fmt.Sprintf("Hello, %v", params.Name), nil
 }
 
 func main() {
-
-	var port int
-	flag.IntVar(&port, "port", 6060, "")
-
-	flag.Parse()
-	Invoke(port)
+	gowrapper.Start(Hello)
 }
 
 
