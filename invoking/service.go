92967ea416f9e345ed99dd8d4a9e0a6fc841c3c0
edkvm <eddy.kiselman@gmail.com>
2019-10-11 20:29:28 +0300

    added a action service

--- invoking/service.go
+++ invoking/service.go
@@ -1,7 +1,29 @@
 package invoking
 
+import "github.com/edkvm/ctrl/action"
 
 type Service interface {
-	RunAction()
-	ListActions()
+	RunAction(name string, params map[string]interface{}) (interface{}, error)
+	ScheduleAction(name string, params map[string]interface{}) (interface{}, error)
 }
+
+type service struct {
+	actionRepo action.ActionRepo
+}
+
+func NewService(actRepo action.ActionRepo, schedRepo action.ActionRepo) *service {
+	return &service{
+		actionRepo: actRepo,
+	}
+}
+
+func (s *service) ScheduleAction(name string, params map[string]interface{}) (interface{}, error) {
+	panic("implement me")
+}
+
+
+
+func (s *service) RunAction(name string, params map[string]interface{}) (interface{}, error) {
+
+}
+
--- invoking/transport.go
+++ invoking/transport.go
@@ -1,1 +1,61 @@
 package invoking
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/zenazn/goji/web"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/edkvm/ctrl/pkg/endpoint"
+	ctrlhttp "github.com/edkvm/ctrl/pkg/http"
+)
+
+
+type invokeRequest map[string]interface{}
+
+
+
+func MakeHandler(srv Service) {
+	r := httprouter.New()
+
+	server := ctrlhttp.NewServer(
+		makeInvokeActionEndpoint(srv),
+		decodeInvokeRequest,
+		encodeInvokeResponse,
+	)
+
+	r.Handler(http.MethodPost, "/action/:name", server)
+
+}
+
+
+func decodeInvokeRequest(_ context.Context, r *http.Request) (interface{}, error){
+	var params map[string]interface{}
+
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		return nil, err
+	}
+
+
+	return params, nil
+}
+
+func encodeInvokeResponse(ctx context.Context, w http.ResponseWriter, resp interface{}) error {
+	return json.NewEncoder(w).Encode(resp)
+}
+
+func makeInvokeActionEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		params := httprouter.ParamsFromContext(ctx)
+		name := params.ByName("name")
+
+		payload := req.(invokeRequest)
+		result, err := s.RunAction(name, payload)
+		if err != nil {
+
+		}
+		return result, nil
+	}
+}
