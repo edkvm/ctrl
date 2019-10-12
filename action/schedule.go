979a957c4c78b6ff092fc245712c6af810ce5040
edkvm <eddy.kiselman@gmail.com>
2019-10-13 00:12:16 +0300

    Added list schedule ep

--- action/schedule.go
+++ action/schedule.go
@@ -15,14 +15,27 @@ type Schedule struct {
 type ScheduleRepo interface {
 	Store(t *Schedule) error
 	FindNext(dur time.Duration) *Schedule
+	FindAllByAction(name string) []*Schedule
 	FindAllByTime(time time.Time) []*Schedule
 }
 
 func NewSchedule(action string, start time.Time) *Schedule {
-
 	return &Schedule{
 		ID:     ScheduleID(genULID()),
 		Action: action,
 		Start:  start,
+		Recurring: false,
 	}
 }
+
+func NewRecurringSchedule(action string, start time.Time, interval int) *Schedule {
+	return &Schedule{
+		ID:     ScheduleID(genULID()),
+		Action: action,
+		Start:  start,
+		Recurring: true,
+		Interval: interval,
+	}
+}
+
+
--- administrating/endpoint.go
+++ administrating/endpoint.go
@@ -3,6 +3,8 @@ package administrating
 import (
 	"context"
 	"github.com/edkvm/ctrl/pkg/endpoint"
+	"log"
+
 	"time"
 )
 
@@ -13,13 +15,50 @@ type scheduleRequest struct {
 	Interval  int
 }
 
-func makeScheduleEndpoint(s Service) endpoint.Endpoint {
+type scheduleResponse struct {
+	ID 		string      `json:"id"`
+	Action    string    `json:"action,omitempty"`
+	Start     time.Time `json:"start,omitempty"`
+	Recurring bool      `json:"recurring"`
+	Interval  int       `json:"interval,omitempty"`
+}
+
+func makeCreateScheduleEndpoint(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		log.Println("dd")
 		schedReq := req.(scheduleRequest)
 		result, err := s.ScheduleRecurringAction(schedReq.Action, schedReq.Interval)
 		if err != nil {
 
 		}
 		return result, nil
+	}
+}
+
+type listSchedule struct {
+	Schedules []scheduleResponse `json:"schedules,omitempty"`
+}
+
+func makeListScheduleEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, req interface{}) (interface{}, error) {
+		name := req.(string)
+
+		result, err := s.ListSchedule(name)
+		if err != nil {
+
+		}
+
+		list := make([]scheduleResponse,  0, len(result))
+		for _, val := range result {
+			list = append(list, scheduleResponse{
+				string(val.ID),
+				val.Action,
+				val.Start,
+				val.Recurring,
+				val.Interval,
+			})
+		}
+
+		return listSchedule{Schedules: list}, nil
 	}
 }
