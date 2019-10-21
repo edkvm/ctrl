0ae75d7096302482dae133b3cfed4d61e24cb6b8
edkvm <edkvmn@gmail.com>
2019-10-21 17:39:24 +0300

    Add docker file

--- invoke/invoke_linux.go
+++ invoke/invoke_linux.go
@@ -9,9 +9,9 @@ import (
 func buildCmd(execName string) *exec.Cmd {
 	// TODO: Add context to cmd
 	cmd := exec.Command(execName)
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
-	}
+	//cmd.SysProcAttr = &syscall.SysProcAttr{
+	//	Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	//}
 
 	return cmd
 }
